2f847c94236043e375a4be144e8366bd1a694ee9
DavidPerezContreras <DavidPerezContreras>
2022-11-24 10:46:50 +0100

    comentado

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+//Para ejecutar este archivo, en Windows o Linux escribimos en la terminal:
+//  #  node main.js
+
+
+
+//es necesario tener node y npm instalado:
+//  En windows, el instalador de node nos instala el gestor de paquetes npm por defecto.
+
+//  En ubuntu, debemos instalar node y npm con apt
+//  #  apt install nodejs 
+//  #  apt install npm
+//  (Podemos instalar ambos en la misma instrucción:   #apt install nodejs npm    )
+
+
+
+
+//Utilizamos la dependencia express 
+//Si nos da un error al ejecutar el programa es muy probable que no tengamos express instalado.
+//Para instalar la dependencia express, ya sea Windows o Linux escribimos en la terminal:
+//  #npm i express    (Es una abrebiación de   #npm install express   Utiliza la que más te guste). 
+
+
 const express = require('express')
 const app = express()
 const port = 3000
